8820b4f4b0d5b83bd1f1bca7c8c055977658ee42
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2024-02-01 16:15:54 +0100

    Implementa jotai

--- src/renderer/src/components/molecules/ButtonsRow.tsx
+++ src/renderer/src/components/molecules/ButtonsRow.tsx
@@ -1,11 +1,24 @@
 import { ComponentProps } from 'react';
 import { Button } from '@/components';
+import { useSetAtom } from 'jotai';
+import { createEmptyNoteAtom, deleteNoteAtom } from '@renderer/store';
 
 export const ButtonsRow = ({ ...attr }: ComponentProps<'div'>) => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom);
+  const deleteNote = useSetAtom(deleteNoteAtom);
+
+  const handleCreation = () => {
+    createEmptyNote();
+  };
+
+  const handleDelete = () => {
+    deleteNote();
+  };
+
   return (
     <div {...attr}>
-      <Button variant="newNote" />
-      <Button variant="deleteNote" />
+      <Button variant="newNote" onClick={handleCreation} />
+      <Button variant="deleteNote" onClick={handleDelete} />
     </div>
   );
 };
--- src/renderer/src/components/molecules/FloatingNoteTitle.tsx
+++ src/renderer/src/components/molecules/FloatingNoteTitle.tsx
@@ -1,13 +1,18 @@
+import { selectedNoteAtom } from '@renderer/store';
+import { useAtomValue } from 'jotai';
 import React, { ComponentProps } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export const FloatingNoteTitle = ({ className, ...attr }: ComponentProps<'div'>) => {
-  const title = 'Note Title';
+  const selectedNote = useAtomValue(selectedNoteAtom);
+
+  if (!selectedNote) return null;
+
   const classNames = twMerge('flex justify-center', className);
 
   return (
     <div className={classNames} {...attr}>
-      <span className="text-gray-400">{title}</span>
+      <span className="text-gray-400">{selectedNote.title}</span>
     </div>
   );
 };
